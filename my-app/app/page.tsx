f7c6c66ee067f0f305a7fa9b2185f26933fd0977
AloPedrero <pedreromartinezalonso@gmail.com>
2026-02-17 21:15:33 -0600

    First commit
    - Project creation
    - Sign in page
    - Sign Up page
    - Home page

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "@/components/LandingPage"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export default async function HomePage() {
+
+  const user = await currentUser()
+  // if no user is logged in, render the public landing page
+  if (!user) return <LandingPage />
+
+  return redirect('/events')
+}
