5c06466f87d2f071b23b7c796c8f1ec6b052dd21
AloPedrero <pedreromartinezalonso@gmail.com>
2026-02-20 01:28:21 -0600

    Added navigation bars
    - fixed tailwindcss v4 configuration

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import PrivateNavBar from "@/components/PrivateNavBar"
+import PublicNavBar from "@/components/PublicNavBar"
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function MainLayout({children}: {children: React.ReactNode}) {
+    const user = await currentUser()
+
+    return (
+        <main className="relative">
+            {/*Render PrivateBar if user exists, otherwise publicnavvar */}
+            {user ? <PrivateNavBar />: <PublicNavBar />}
+
+            {/*Render the children */}
+            <section className="pt-36">
+                {children}
+            </section>
+        </main>
+    )
+}
