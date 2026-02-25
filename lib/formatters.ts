1fe1d6c782c91935ef7ece07e6a7cd0026554f4b
AloPedrero <pedreromartinezalonso@gmail.com>
2026-02-25 02:22:14 -0600

    Created Event cards in the events page

---
+++ lib/formatters.ts
@@ -0,0 +1,17 @@
+// Formats a duration in minutes into a readable string like "1hr 30 mins"
+export function formatEventDescription(durationInMinutes: number) : string {
+    const hours = Math.floor(durationInMinutes / 60) //Get number of full hours
+    const minutes = durationInMinutes % 60 // get remaining minutes after removing full hours
+
+    //format minutes string (e.g., "1min" or "10 mins")
+    const minutesString = `${minutes} ${minutes > 1 ? "mins" : "min"}`
+    //format minutes string (e.g., "1min" or "10 mins")
+    const hoursString = `${hours} ${hours > 1 ? "hrs" : "hr"}`
+
+    //Return only minutes if there are no full hours
+    if (hours === 0) return minutesString
+    // Return only hours if there are no extra minutes
+    if (minutes === 0) return hoursString
+    //return both hours and minutes if both are present
+    return `${hoursString} ${minutesString}`
+}
