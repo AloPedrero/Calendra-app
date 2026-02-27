b33407eb5030cb884752e1eb52ad4e05175de8df
AloPedrero <pedreromartinezalonso@gmail.com>
2026-02-27 00:35:32 -0600

    feat: Created the book route

--- app/(main)/(public)/book/page.tsx
+++ app/(main)/(public)/book/page.tsx
@@ -1,7 +1,22 @@
-export default function Page() {
-    return(
-        <h1>
-            Book page
-        </h1>
-    )
+'use client'
+
+import Loading from "@/components/Loading";
+import { useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default function PublicPage() {
+    const {user, isLoaded} = useUser() //Using the isloaded to check if user data is available
+
+    if (!isLoaded) {
+        //Display loading until user data is loading
+        return <Loading />
+    }
+        
+    if (!user) {
+        // Redirect to login if no user is found
+        return redirect('/login')
+    }
+
+    // Once user is available, redirect to the booking page [public Profile Page]
+    return redirect(`/book/${user.id}`)
 }
